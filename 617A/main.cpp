51759bd577ec280d18b2bc0c544d7c665adb5ccb
the-onewho-knocks <borsehardik@gmail.com>
2026-08-23 17:03:02 +0530

    feat(codeforces-617a): Implement Elephant problem solution
    This commit provides the initial solution for Codeforces problem 617A. The `main.cpp` calculates the minimum number of steps an elephant needs to travel a given distance. Input, output, and expected files were updated for testing and validation of the implemented logic.

---
+++ 617A/main.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(){
+    int a ;
+    cin>>a;
+
+    cout<<(a + 4) / 5 <<endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    solve();
+
+    return 0;
+}
