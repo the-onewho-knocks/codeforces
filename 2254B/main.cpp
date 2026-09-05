765405f74149fb8792350491fa2bdc6fb8d34dc9
the-onewho-knocks <borsehardik@gmail.com>
2026-09-05 17:54:25 +0530

    feat(2254B): Implement solution for problem 2254B
    Refined the algorithm in main.cpp to correctly solve the problem statement. This update includes modifications to input.txt, expected.txt, and output.txt for testing, and a recompiled main.exe.

---
+++ 2254B/main.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+    int t ;
+    cin>>t;
+
+    while(t --){
+        int n;
+        string s;
+
+        cin>>n>>s;
+
+        int block = 1;
+
+        for(int i = 1 ; i < n ; ++i){
+            if(s[i - 1] != s[i]){
+                block++;
+            }
+        }
+
+        int best = 0;
+
+        for(int i = 1 ; i < n - 1 ; ++i){
+            if(s[i - 1] != s[i] && s[i+1] != s[i]){
+
+                if(s[i - 1] == s[i + 1]){
+                    best = max(best , 2);
+                }
+                else{
+                    best = max(best , 1);
+                }
+            }
+        }
+
+        cout<<block - best<<endl;
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    solve();
+
+    return 0;
+}
